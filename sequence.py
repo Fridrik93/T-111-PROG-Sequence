bdd56154db11c9f706ab114cfbc3fe53715ce174
Fridrik93 <fridrik19@ru.is>
2019-09-05 15:30:57 +0000

    Algorithm written for sequence

--- sequence.py
+++ sequence.py
@@ -1,1 +1,14 @@
 n = int(input("Enter the length of the sequence: ")) # Do not change this line
+n1 = 1
+n2 = 2
+n3 = 3
+
+for i in range(1, n+1):
+    if 1 <= i <= 3:
+        print(i)
+    else:
+        total = n1 + n2 + n3
+        n1 = n2
+        n2 = n3
+        n3 = total
+        print(total)
